cb9e2b16c25fe1f2f592996fb8242d2b40836e52
noxsios <harryrndzz@gmail.com>
2020-11-22 05:11:16 +0100

    Formatted code, regenerated manifest

--- ps-menu.psm1
+++ ps-menu.psm1
@@ -1,12 +1,11 @@
 function DrawMenu {
-    param ($menuItems, $menuPosition, $Multiselect, $selection)
-    $l = $menuItems.length
-    for ($i = 0; $i -le $l;$i++) {
-		if ($menuItems[$i] -ne $null){
+	param ($menuItems, $menuPosition, $Multiselect, $selection)
+	$l = $menuItems.length
+	for ($i = 0; $i -le $l; $i++) {
+		if ($null -ne $menuItems[$i]) {
 			$item = $menuItems[$i]
-			if ($Multiselect)
-			{
-				if ($selection -contains $i){
+			if ($Multiselect) {
+				if ($selection -contains $i) {
 					$item = '[x] ' + $item
 				}
 				else {
@@ -14,18 +13,19 @@ function DrawMenu {
 				}
 			}
 			if ($i -eq $menuPosition) {
-				Write-Host "> $($item)" -ForegroundColor Green
-			} else {
+				Write-Host "» $($item)" -ForegroundColor Green
+			}
+			else {
 				Write-Host "  $($item)"
 			}
 		}
-    }
+	}
 }
 
-function Toggle-Selection {
+function ToggleSelection {
 	param ($pos, [array]$selection)
-	if ($selection -contains $pos){ 
-		$result = $selection | where {$_ -ne $pos}
+	if ($selection -contains $pos) { 
+		$result = $selection | Where-Object { $_ -ne $pos }
 	}
 	else {
 		$selection += $pos
@@ -35,49 +35,44 @@ function Toggle-Selection {
 }
 
 function Menu {
-    param ([array]$menuItems, [switch]$ReturnIndex=$false, [switch]$Multiselect)
-    $vkeycode = 0
-    $pos = 0
-    $selection = @()
-    [console]::CursorVisible=$false #prevents cursor flickering
-    if ($menuItems.Length -gt 0)
-	 {
+	param ([array]$menuItems, [switch]$ReturnIndex = $false, [switch]$Multiselect)
+	$vkeycode = 0
+	$pos = 0
+	$selection = @()
+	[console]::CursorVisible = $false #prevents cursor flickering
+	if ($menuItems.Length -gt 0) {
 		DrawMenu $menuItems $pos $Multiselect $selection
 		While ($vkeycode -ne 13 -and $vkeycode -ne 27) {
 			$press = $host.ui.rawui.readkey("NoEcho,IncludeKeyDown")
 			$vkeycode = $press.virtualkeycode
-			If ($vkeycode -eq 38 -or $press.Character -eq 'k') {$pos--}
-			If ($vkeycode -eq 40 -or $press.Character -eq 'j') {$pos++}
-			If ($press.Character -eq ' ') { $selection = Toggle-Selection $pos $selection }
-			if ($pos -lt 0) {$pos = 0}
-			If ($vkeycode -eq 27) {$pos = $null }
-			if ($pos -ge $menuItems.length) {$pos = $menuItems.length -1}
-			if ($vkeycode -ne 27)
-			{
-			   $startPos = [System.Console]::CursorTop - $menuItems.Length
+			if ($vkeycode -eq 38 -or $press.Character -eq 'k') { $pos-- }
+			if ($vkeycode -eq 40 -or $press.Character -eq 'j') { $pos++ }
+			if ($press.Character -eq ' ') { $selection = ToggleSelection $pos $selection }
+			if ($pos -lt 0) { $pos = 0 }
+			if ($vkeycode -eq 27) { $pos = $null }
+			if ($pos -ge $menuItems.length) { $pos = $menuItems.length - 1 }
+			if ($vkeycode -ne 27) {
+				$startPos = [System.Console]::CursorTop - $menuItems.Length
 				[System.Console]::SetCursorPosition(0, $startPos)
 				DrawMenu $menuItems $pos $Multiselect $selection
 			}
 		}
 	}
-	else 
-	{
+	else {
 		$pos = $null
 	}
-    [console]::CursorVisible=$true
+	[console]::CursorVisible = $true
 
-    if ($ReturnIndex -eq $false -and $pos -ne $null)
-	{
-		if ($Multiselect){
+	if ($ReturnIndex -eq $false -and $null -ne $pos) {
+		if ($Multiselect) {
 			return $menuItems[$selection]
 		}
 		else {
 			return $menuItems[$pos]
 		}
 	}
-	else 
-	{
-		if ($Multiselect){
+	else {
+		if ($Multiselect) {
 			return $selection
 		}
 		else {
